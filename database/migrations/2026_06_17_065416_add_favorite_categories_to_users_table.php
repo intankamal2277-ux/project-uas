58ca53f2202e0d59ad526e35b0b9f655e7bb915f
bila-boop <251351019@wastukancana.ac.id>
2026-06-17 14:58:11 +0700

    edit 2 add favorit

--- database/migrations/2026_06_17_065416_add_favorite_categories_to_users_table.php
+++ database/migrations/2026_06_17_065416_add_favorite_categories_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('favorite_categories');
+            $table->json('favorite_categories')->nullable()->after('password');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('favorite_categories');
         });
     }
 };
