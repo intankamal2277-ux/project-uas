70c7399c76c9389a3bc5ff66c41eb0a65190fd2e
restyputri242-maker <restyputri242@gmail.com>
2026-06-24 14:17:05 +0700

    edit 3 admin controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\SavedNews;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function index()
+    {
+        $stats = [
+            'total_users' => User::count(),
+            'total_comments' => Comment::count(),
+            'total_saved_news' => SavedNews::count(),
+            'total_admins' => User::where('role', 'admin')->count(),
+        ];
+
+        $users = User::withCount(['comments', 'savedNews'])
+            ->latest()
+            ->get();
+
+        return view('admin.dashboard', compact('stats', 'users'));
+    }
 }
