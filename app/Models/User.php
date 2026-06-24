5876e7c4a7a8457bd91e4a9827e6511ac1d94995
intankamal2277-ux <intankamal2277@gmail.com>
2026-06-24 14:33:33 +0700

    edit 20 models user php

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,8 @@ class User extends Authenticatable
     {
         return $this->hasMany(SavedNews::class);
     }
+        public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
 }
